4d442ee05a6dc267e22cdaeb7a574c38ab8c7387
JLee <thishereisjenn@gmail.com>
2021-11-22 20:38:27 -0800

    added time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-let scheduleElArray = [];                  
+let scheduleElArray = [
+    schedule9am,
+    schedule10am,
+    schedule11am,
+    schedule12pm,
+    schedule1pm,
+    schedule2pm,
+    schedule3pm,
+    schedule4pm,
+    schedule5pm,
+];                  
+
+let schedule9am = $("#9AM");
+let schedule10am = $("#10AM");
+let schedule11am = $("#11AM");
+let schedule12pm = $("#12PM");
+let schedule1pm = $("#1PM");
+let schedule2pm = $("#2PM");
+let schedule3pm = $("#3PM");
+let schedule4pm = $("#4PM");
+let schedule5pm = $("#5PM");
 
 function updatedTime() {
     let today = moment();
@@ -17,6 +37,5 @@ function renderLastRegistered() {
 renderLastRegistered();
 updatedTime();
 setInterval(updatedTime, 1000);
-
 
 let containerEl = $(".container");
